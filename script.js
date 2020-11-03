ead913650a1baab1b8af2231cee252d51a8fbdcf
kovacsbenc3 <73656785+kovacsbenc3@users.noreply.github.com>
2020-11-03 14:38:40 +0100

    Greeter names

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("This is JaBaScript;)");
+
+let colors = ["orange", "blue", "green", "purple"];
+
+let post = {
+    title: "My Post",
+    description: "My First post, awesome",
+    likeCount: 12,
+    comments: ["cool","nice job"]
+};
+
+console.log(colors);
+console.log(post);
+
+let age = 18;
+
+if (age>=18){
+    console.log("You are an adult!");
+} else{
+    console.log("you r a kid");
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);   
+}
+
+console.log(colors);
+
+let myFunction = x =>{
+    console.log(x);
+}
+
+let greeter = name => {
+    console.log(`Hello, ${name}`);
+    if(name ==="Adam") {
+        console.log("he's an adult");
+    }
+    if (name === "Adam"){
+        console.log("he's an adult");
+    }
+}
+
+greeter("Adam");
+greeter("Peter");
+greeter("Mate");
+greeter("Reka");
+
+colors.forEach(color => {
+console.log(color);
+});
+
+console.log("END OF THE CODE");
+
